bd6dbfe9e63dee17931929188034e2376187d004
leandrocarra-luizalabs <leandrocarra@luizalabs.com>
2020-06-04 00:03:06 -0300

    routes and seeds

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import Knex from './database/connections';
 
 const routes = express.Router();
 
 routes.get('/', (req,res) => {
   return res.send('oi')
+});
+
+routes.get('/items', async (req, res) => {
+  const items = await Knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 import routes from './routes';
 
- const app = express();
+const app = express();
 
- app.use(express.json());
+app.use(express.json());
 app.use(routes);
 
- app.listen(3333);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333);
